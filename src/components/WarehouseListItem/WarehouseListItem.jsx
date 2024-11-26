9a9fc7127fbe4b480712e4bc5c9f31147e0b50be
katie1708 <katienguyen3012@gmail.com>
2024-11-25 21:27:51 -0800

    Deconstruct props, fix address data, add font sizes to global.scss file, edit some styling

--- src/components/WarehouseListItem/WarehouseListItem.jsx
+++ src/components/WarehouseListItem/WarehouseListItem.jsx
@@ -6,29 +6,39 @@ import './WarehouseListItem.scss'
 
 
 export function MobileListItem(props) {
-    const warehouse = props.warehouse;
+    const {
+        id,
+        warehouse_name,
+        address,
+        city,
+        country,
+        contact_name,
+        contact_position,
+        contact_phone,
+        contact_email
+    } = props.warehouse;
 
     return(
         <ul className="mobile-warehouse">
             <li className="mobile-warehouse__item">
                 <p className="mobile-warehouse__item--title">WAREHOUSE</p>
-                <Link to={`/warehouses/${warehouse.id}`}>
-                    {warehouse.warehouse_name}
+                <Link className="mobile-warehouse__item--link" to={`/warehouses/${id}`}>
+                    {warehouse_name}
                     <img src={chevron}/>
                 </Link>
             </li>
             <li className="mobile-warehouse__item">
                 <p className="mobile-warehouse__item--title">CONTACT NAME</p>
-                <p>{warehouse.contact_name}</p>
+                <p>{contact_name}</p>
             </li>
             <li className="mobile-warehouse__item">
                 <p className="mobile-warehouse__item--title">ADDRESS</p>
-                <p>{warehouse.address}</p>
+                <p>{`${address}, ${city}, ${country}`}</p>
             </li>
             <li className="mobile-warehouse__item">
                 <p className="mobile-warehouse__item--title">CONTACT INFORMATION</p>
-                <p>{warehouse.contact_phone}</p>
-                <p>{warehouse.contact_email}</p>
+                <p>{contact_phone}</p>
+                <p>{contact_email}</p>
             </li>
             <li className="mobile-warehouse__item">
                 <img src={delete_icon} />
@@ -41,20 +51,30 @@ export function MobileListItem(props) {
 }
 
 export function DesktopListItem(props) {
-    const warehouse = props.warehouse;
+    const {
+        id,
+        warehouse_name,
+        address,
+        city,
+        country,
+        contact_name,
+        contact_position,
+        contact_phone,
+        contact_email
+    } = props.warehouse;
     return(
         <ul className="desktop-warehouse__row">
             <li className="desktop-warehouse__row--1">
-                <Link to={`/warehouses/${warehouse.id}`}>
-                    {warehouse.warehouse_name}
+                <Link to={`/warehouses/${id}`}>
+                    {warehouse_name}
                     <img src={chevron}/>
                 </Link>
             </li>
-            <li className="desktop-warehouse__row--2">{warehouse.address}</li>
-            <li className="desktop-warehouse__row--3">{warehouse.contact_name}</li>
+            <li className="desktop-warehouse__row--2">{`${address}, ${city}, ${country}`}</li>
+            <li className="desktop-warehouse__row--3">{contact_name}</li>
             <li className="desktop-warehouse__row--4">
-                <p>{warehouse.contact_phone}</p>
-                <p>{warehouse.contact_email}</p>
+                <p>{contact_phone}</p>
+                <p>{contact_email}</p>
             </li>
             <li className="desktop-warehouse__row--5">
                 <img src={delete_icon} />
